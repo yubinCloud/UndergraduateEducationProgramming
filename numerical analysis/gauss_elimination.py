77b4833e70a4b70d13ce04d6910fbc1ff91dc4fe
yubinCloud <yubin_SkyWalker@yeah.net>
2020-06-01 17:30:53 +0800

    add Elimination of column pivot entries

--- numerical analysis/gauss_elimination.py
+++ numerical analysis/gauss_elimination.py
@@ -18,11 +18,12 @@ def augment_matrix(A, b):
     return np.hstack((A, b))
 
 
-def gauss_elimination(A, b):
+def gauss_elimination(A, b, need_primecol=False):
     """
     高斯消去法
     :param A: 系数矩阵
     :param b: 值矩阵
+    :param need_primecol: 是否采用列主元策略
     :return: 消元后的矩阵
     """
     B = augment_matrix(A, b)  # 生成增广矩阵
@@ -31,6 +32,10 @@ def gauss_elimination(A, b):
     print(B)
     for cur_row in range(0, num_row - 1):
         print("第{}轮：".format(cur_row + 1))
+        # 若需要交换列主元则进行相关处理
+        if (need_primecol):
+            primecol_strategy(B, cur_row, cur_row)
+
         for row in range(cur_row + 1, num_row):
             coef = - B[row, cur_row] / B[cur_row, cur_row]
             B[row, :] = B[row] + coef * B[cur_row]
@@ -40,11 +45,40 @@ def gauss_elimination(A, b):
     return B
 
 
+def primecol_strategy(B, cur_row, cur_col):
+    """
+    列主元策略
+    :param B:
+    :return:
+    """
+    primecol_row = find_primecol(B, cur_row, cur_col)  # 列主元所在行
+    print("交换row{}与row{}".format(cur_row + 1, primecol_row + 1))
+    B[[primecol_row, cur_row], :] = B[[cur_row, primecol_row]]  # 交换当前行与列主元所在行
+
+
+def find_primecol(M, cur_row, cur_col):
+    """
+    寻找自row行及下面行中第col列的最大元所在行
+    :param M: 所要处理的矩阵
+    :param cur_row: 行
+    :param cur_col: 列
+    :return: 列主元所在行
+    """
+    num_row = M.shape[0]
+    max_value = M[cur_row, cur_col]
+    max_row = cur_row
+    for row in range(cur_row + 1, num_row):
+        if M[row, cur_col] > max_value:
+            max_value = M[row, cur_col]
+            max_row = row
+    return max_row
+
+
 if __name__ == '__main__':
     A = np.array([
-        [r(2), r(-1), r(3)],
-        [r(4), r(2), r(5)],
-        [r(1), r(2), r(0)]
+        [r(2), r(2), r(3)],
+        [r(4), r(7), r(7)],
+        [r(-2), r(4), r(5)]
     ])
-    b = np.array([r(1), r(4), r(7)])
-    gauss_elimination(A, b)
+    b = np.array([r(3), r(1), r(-7)])
+    gauss_elimination(A, b, need_primecol=True)
